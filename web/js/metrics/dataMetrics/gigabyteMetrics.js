16fe3c6df22134d62b4800c05a6537de8b0cdd90
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-16 10:26:50 -0300

    classes convert de gigabyte e terabyte

---
+++ web/js/metrics/dataMetrics/gigabyteMetrics.js
@@ -0,0 +1,23 @@
+function gigabyteToByte(number){
+    number * 1000000000
+}
+
+function gigabyteToKilobyte(number){
+    return number * 1000000
+}
+
+function gigabyteToQuiloocteto(number){
+    return number * 976563
+}
+
+function gigabyteToMegabyte(number){
+    return number * 1000
+}
+
+function gigabyteToTerabyte(number){
+    return number / 1000
+}
+
+function gigabyteToPetabyte(number){
+    return number / 1000000
+}
---
+++ web/js/metrics/dataMetrics/terabyteMetrics.js
@@ -0,0 +1,23 @@
+function terabyteToByte(number){
+    return number / 1000000000000
+}
+
+function terabyteToKilobyte(number){
+    return number / 1000000000
+}
+
+function terabyteToQuiloocteto(number){
+    return number / 9.76600000000
+}
+
+function terabyteToMegabyte(number){
+    return number * 1000000
+}
+
+function terabyteToGigabyte(number){
+    return number * 1000
+}
+
+function terabyteToPetabyte(number){
+    return number / 1000
+}
