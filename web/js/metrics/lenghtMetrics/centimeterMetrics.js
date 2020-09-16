43ff6cf588553ca86664e4c026c3365f57ac322d
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-16 18:24:19 -0300

    add class centimeterMetrics

---
+++ web/js/metrics/lenghtMetrics/centimeterMetrics.js
@@ -0,0 +1,39 @@
+function centimeterTokilometer(number){
+    return number / 100000
+}
+
+function centimeterToMeter(number){
+    return number / 100
+}
+
+function centimeterToMillimeter(number){
+    return number * 10
+}
+
+function centimeterToMicrometer(number){
+    return number * 10000
+}
+
+function centimeterToNanometer(number){
+    return number * 10000000
+}
+
+function centimeterToMile(number){
+    return number / 160934
+}
+
+function centimeterToYard(number){
+    return (number / 91.44).toFixed(6)
+}
+
+function centimeterToFeet(number){
+    return (number / 30.48).toFixed(6)
+}
+
+function centimeterToInch(number){
+    return (number / 2.54).toFixed(5)
+}
+
+function centimeterToNauticalMile(number){
+    return (number / 185200).toFixed(9)
+}
