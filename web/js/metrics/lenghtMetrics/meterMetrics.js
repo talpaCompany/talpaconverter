7e94d0819e7793e1b63a3c417ef7228f13a6606c
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-16 18:08:27 -0300

    add class meterMetrics

---
+++ web/js/metrics/lenghtMetrics/meterMetrics.js
@@ -0,0 +1,39 @@
+function meterToKilometer(number){
+    return number / 1000
+}
+
+function meterToCentimeter(number){
+    return number * 100
+}
+
+function meterToMillimeter(number){
+    return number * 1000
+}
+
+function meterToMicrometer(number){
+    return number * 1000000
+}
+
+function meterToNanometer(number){
+    return number * 1000000000
+}
+
+function meterToMile(number){
+    return (number / 1609).toFixed(7)
+}
+
+function meterToYard(number){
+    return number * 1.094  
+}
+
+function meterToFeet(number){
+    return number * 3.281
+}
+
+function meterToInch(number){
+    return number * 39.37
+}
+
+function meterToNauticalMile(number){
+    return (number / 1852).toFixed(7)
+}
