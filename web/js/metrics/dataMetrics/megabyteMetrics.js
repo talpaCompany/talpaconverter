b9ecc46fe43498553d336552a35223351a4fa6ce
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-15 22:00:05 -0300

    add classe magabytemetrics

---
+++ web/js/metrics/dataMetrics/megabyteMetrics.js
@@ -0,0 +1,23 @@
+function megabyteToByte(number){
+    return number * 1000000
+}
+
+function megabyteToKilobyte(number){
+    return number * 1000
+}
+
+function megabyteToQuiloocteto(number){
+    return number * 977
+}
+
+function megabyteToGigabyte(number){
+    return number / 1000
+}
+
+function megabyteToTerabyte(number){
+    return number / 1000000
+}
+
+function megabyteToPetabyte(number){
+    return number / 1000000000
+}
