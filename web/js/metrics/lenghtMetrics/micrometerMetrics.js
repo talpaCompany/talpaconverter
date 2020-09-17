3445cbd4e9903561374bfaa7b89044be5e82beb7
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-17 17:40:14 -0300

    add class micrometerMetrics.js

---
+++ web/js/metrics/lenghtMetrics/micrometerMetrics.js
@@ -0,0 +1,39 @@
+function micrometerToKilometer(number){
+    return number / 1000000000
+}
+
+function micrometerToMeter(number){
+    return number / 1000000
+}
+
+function micrometerToCentimeter(number){
+    return number / 10000
+}
+
+function micrometerToMillimeter(number){
+    return number / 1000
+}
+
+function micrometerToNanometer(number){
+    return number * 1000
+}
+
+function micrometerToMile(number){
+    number / 1609000000000
+}
+
+function micrometerToYard(number){
+    return number / 914400
+}
+
+function micrometerToFeet(number){
+    return number / 304800
+}
+
+function micrometerToInch(number){
+    return number / 25400
+}
+
+function micrometerToNauticalMile(number){
+    return number / 1852000000000
+}
