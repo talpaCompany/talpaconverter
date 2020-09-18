6448bf2f9cd11200fcf19decfe838a3a57d85c47
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-17 23:22:46 -0300

    add class americanGallonMetrics.js

---
+++ web/js/metrics/fuelMetrics/americanGallonMetrics.js
@@ -0,0 +1,11 @@
+function milePerAmericanGallonToMilePerImperialGallon(number){
+    return number * 1.201
+}
+
+function milePerAmericanGallonToKilometerPerLiter(number){
+    return number / 2.352
+}
+
+function milePerAmericanGallonToLiterPer100Kilometers(number){
+    return 235.215 / number
+}
