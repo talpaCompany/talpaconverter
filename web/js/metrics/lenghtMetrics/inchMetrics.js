08978b4534bff4e4502c48a11c75b354ebcbf39b
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-17 23:07:51 -0300

    add class inchMetrics.js

---
+++ web/js/metrics/lenghtMetrics/inchMetrics.js
@@ -0,0 +1,39 @@
+function inchToKilometer(number){
+    return number / 39370
+}
+
+function inchToMeter(number){
+    return number / 39.37
+}
+
+function inchToCentimeter(number){
+    return number * 2.54
+}
+
+function inchToMillimeter(number){
+    return number * 25.4
+}
+
+function inchToMicrometer(number){
+    return number * 25400
+}
+
+function inchToNanometer(number){
+    return number * 2540000000
+}
+
+function inchToMile(number){
+    return number / 63360
+}
+
+function inchToYard(number){
+    return number / 36
+}
+
+function inchToFeet(number){
+    return number / 12
+}
+
+function inchToNauticalMile(number){
+    return number / 72913
+}
