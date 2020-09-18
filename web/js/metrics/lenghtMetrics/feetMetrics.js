99f44fcb38fc16438dd23c6cd8c3ad7776a888a9
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-17 23:02:04 -0300

    add class feetMetrics.js

---
+++ web/js/metrics/lenghtMetrics/feetMetrics.js
@@ -0,0 +1,39 @@
+function feetToKilometer(number){
+    return number / 3281
+}
+
+function feetToMeter(number){
+    return number / 3.281
+}
+
+function feetToCentimeter(number){
+    return number * 30.48
+}
+
+function feetToMillimeter(number){
+    return number * 305
+}
+
+function feetToMicrometer(number){
+    return number * 304800
+}
+
+function feetToNanometer(number){
+    return number * 304800000000
+}
+
+function feetToMile(number){
+    return number / 5280
+}
+
+function feetToYard(number){
+    return number / 3
+}
+
+function feetToInch(number){
+    return number * 12
+}
+
+function feetToNauticalMile(number){
+    return number / 6076
+}
