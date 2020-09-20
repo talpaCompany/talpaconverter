1a4d15849a13bef7129d17f74e995358b763d158
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-20 12:34:52 -0300

    add class gramcalorie and att index.js

--- web/js/metrics/energyMetrics/index.js
+++ web/js/metrics/energyMetrics/index.js
@@ -1,14 +1,17 @@
 import * as joule from './jouleMetrics.js'
 import * as kilojoule from './kilojouleMetrics.js'
+import * as gramcalorie from './gramcalorieMetrics.js'
 
 group.energy = "energy"
 
 types.energy = {
     joule : ["joule", "J"],
-    kilojoule : ["kilojoule", "KJ"]
+    kilojoule : ["kilojoule", "KJ"],
+    gramcalorie : ["gramcalorie", "gCal"]
 }
 
 export default {
     ...joule.default,
-    ...kilojoule.default
+    ...kilojoule.default,
+    ...gramcalorie.default
 }
