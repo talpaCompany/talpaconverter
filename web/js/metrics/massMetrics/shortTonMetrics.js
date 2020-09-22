07926805b06fd0301f1144707bcfa312e606ec4f
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-09-22 19:00:23 -0300

    add class shortTonMetrics.js

---
+++ web/js/metrics/massMetrics/shortTonMetrics.js
@@ -0,0 +1,13 @@
+function shortTonToTon(number){
+    return (number / 1.102).toFixed(6)
+}
+
+function shortTonToKilogram(number){
+    return number * 907
+}
+
+shortTonToTon(1)
+
+/*export default {
+    shortTonToTon
+}*/
